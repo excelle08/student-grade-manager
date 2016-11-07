99ef190e71341082372dfe82503f58447c87ec55
excelle08 <suwei779@126.com>
2016-11-07 23:06:50 +0800

    Teacher

--- static/js/teacher.js
+++ static/js/teacher.js
@@ -1,25 +1,87 @@
 $(document).ready(loadTeacherView);
 
-function loadTeacherView () {
-    var params = getArgs();
+var classes = [
+    {
+        id: 1,
+        number: 2014060104,
+        course: {
+            id: 23,
+            student_count: 2
+        },
+        students: [
+            {
+                id: 2014060104001,
+                name: 'Tom',
+                gender: 1,
+                major: '计算机',
+                regular: 0,
+                midterm: 0,
+                final: 0,
+                total: 0,
+                grade: 0
+            },
+            {
+                id: 2014060104002,
+                name: 'Jerry',
+                gender: 1,
+                major: '计算机',
+                regular: 0,
+                midterm: 0,
+                final: 0,
+                total: 0,
+                grade: 0
+            }
+        ]
 
-    getApi('/api/teacher', {}, function (err, teacher) {
-        if (err) {
-            alert('Error loading teacher info. - ' + err.message);
-        } else {
-            getApi('/api/teacher/class', {
-                page: params.page || 1
-            }, function (err, classes) {
-                if (err) {
-                    alert('Error loading classes. - ' + err.message);
-                } else {
-                    gradeView(classes);
-                    ratingView(classes);
-                    infoView(teacher);
-                }
-            });
-        }
-    });
+    },
+    {
+        id: 2,
+        number: 2014040203,
+        course: {
+            id: 24,
+            student_count: 2
+        },
+        students: [
+            {
+                id: 2014040203001,
+                name: 'Tom',
+                gender: 1,
+                major: '挖掘机',
+                regular: 0,
+                midterm: 0,
+                final: 0,
+                total: 0,
+                grade: 0
+            },
+            {
+                id: 2014040203002,
+                name: 'Jerry',
+                gender: 1,
+                major: '挖掘机',
+                regular: 0,
+                midterm: 0,
+                final: 0,
+                total: 0,
+                grade: 0
+            }
+        ]
+    }
+];
+
+var teacher = {
+    name: '杨宏春',
+    id: 12345,
+    level: 'Professor',
+    address: '主楼A5-233',
+    telephone: '13800012345',
+    email: 'yhc@uestc.edu.cn',
+    intro: '量子计算机'
+};
+
+function loadTeacherView () {
+    gradeView(classes);
+    ratingView(classes);
+    infoView(teacher);
 }
 
 function gradeView (classes) {
@@ -28,7 +90,9 @@ function gradeView (classes) {
         data: {
             classes: classes,
             my_page: 1,
-            students: []
+            students: [],
+            class: {},
+            current_class: {}
         },
         computed: {
             page: {
@@ -37,16 +101,6 @@ function gradeView (classes) {
                 },
                 set: function (val) {
                     this.my_page = val;
-                    var _this = this;
-                    getApi('/api/teacher/class', {
-                        page: this.my_page
-                    }, function (err, r) {
-                        if (err) {
-                            alert('Error paging: ' + err.message);
-                        } else {
-                            reloadArray(_this.classes, r);
-                        }
-                    });
                 }
             }
         },
@@ -60,17 +114,29 @@ function gradeView (classes) {
             next: function () {
                 this.page = this.page + 1;
             },
-            setGrade: function (class) {
-                $('#modal-set-grade').modal('show');
-                getApi('/api/grades', {
+            setGrade: function (class_) {
+                $('#modal-set-grades').modal('show');
+                this.students = class_.students;
+                this.current_class = class_.course;
+            },
+            displayStat: function (class_) {
+                $('#modal-visualize-grades').modal('show');
+                this.students = class_.students;
+                this.current_class = class_.course;
+            },
+            computeTotal: function (student) {
+                var regular = parseInt($('#stu-regular-score-' + student.id).val() || '0');
+                var midterm = parseInt($('#stu-midterm-score-' + student.id).val() || '0');
+                var final = parseInt($('#stu-final-score-' + student.id).val() || '0');
+                var total = parseInt(regular * 0.1 + midterm * 0.2 + final * 0.7);
 
-                })
+                student.regular = regular;
+                student.midterm = midterm;
+                student.final = final;
+                student.total = total;
             },
-            displayStat: function (class) {
-                $('#modal-visualize-grade').modal('show');
-            },
-            editStuGrade: function (student) {
-                
+            editStuGrade: function () {
+
             },
             import: function () {
 
@@ -97,16 +163,6 @@ function ratingView (classes) {
                 },
                 set: function (val) {
                     this.my_page = val;
-                    var _this = this;
-                    getApi('/api/teacher/class', {
-                        page: this.my_page
-                    }, function (err, r) {
-                        if (err) {
-                            alert('Error paging: ' + err.message);
-                        } else {
-                            reloadArray(_this.classes, r);
-                        }
-                    });
                 }
             }
         },
@@ -120,7 +176,7 @@ function ratingView (classes) {
             next: function () {
                 this.page = this.page + 1;
             },
-            displayRatingStat: function (class) {
+            displayRatingStat: function (class_) {
                 $('#modal-visualize-ratings').modal('show');
             }
         }
@@ -141,19 +197,7 @@ function infoView (teacher) {
         },
         methods: {
             updateTeacherInfo: function () {
-                postApi('/api/teacher', {
-                    id: this.teacher_id,
-                    address: this.address,
-                    telephone: this.telephone,
-                    email: this.email,
-                    intro: this.intro
-                }, function (err, r) {
-                    if (err) {
-                        alert('Error: ' + err.message);
-                    } else {
-                        alert('修改成功！');
-                    }
-                });
+                alert('修改成功!');
             }
         }
     })
